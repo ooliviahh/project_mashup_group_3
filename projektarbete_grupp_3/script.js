91ba3b34c2eb64cf5ae9077d1fb69d593b92f546
ooliviahh <h22oliha@du.se>
2024-05-07 13:00:31 +0200

    trial and error 3

--- projektarbete_grupp_3/script.js
+++ projektarbete_grupp_3/script.js
@@ -1,6 +1,6 @@
-const urlSLU1 = "https://api.scb.se/OV0104/v1/doris/sv/ssd/START/MI/MI0603/MI0603D/DjuroVaxtart";
+const urlDjuroVaxtart = "https://api.scb.se/OV0104/v1/doris/sv/ssd/START/MI/MI0603/MI0603D/DjuroVaxtart";
 
-const querySLU1 = {
+const queryDjuroVaxtart = {
   "query": [
     {
       "code": "Region",
@@ -12,29 +12,73 @@ const querySLU1 = {
       }
     },
     {
-      "code": "Arter",
+      "code": "Tid",
       "selection": {
         "filter": "item",
         "values": [
-          "Daggdjur"
+          "2022"
         ]
       }
     }
   ],
   "response": {
-    "format": "JSON"
+    "format": "json"
   }
-}
+};
 
-const request = new Request(urlSLU1, {
-  method: "POST", 
- body: JSON.stringify(querySLU1)
-                });
-        
+const request = new Request(urlDjuroVaxtart, {
+  method: 'POST',
+  body: JSON.stringify(queryDjuroVaxtart)
+});
+
 fetch(request)
 .then(response => response.json())
-.then((djurData) =>  {
-console.log(djurData);    });
+.then((dataDjuroVaxtart) => {
+
+
+  console.log(dataDjuroVaxtart);
+
+  const values = dataDjuroVaxtart.data.map((value) => value.values[0]);
+  console.log("värden", values);
+
+  const labels = dataDjuroVaxtart.data.map(value => value.key[1]);
+  console.log("etiketter:", labels);
+
+    const datasets = [
+      {
+        label: "Antal arter, utrotningshotade djur i Sverige",
+        data: values
+      }
+    ];
+
+    const data = {
+      labels,
+      datasets
+    };
+
+    console.log("data:", data);
+
+    const config = {type: "bar", data}
+    const canvas = document.getElementById('djuroVaxtarter');
+    const djuroVaxtarterChart = new Chart(canvas, config);
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 // window.addEventListener('scroll', function() {
